a0f74759c3d3f16b88be017ae9cb210e1ce2ddf0
adamvinueza <adamvinueza@pm.me>
2020-12-31 16:47:22 -0700

    finish_trace, testing, logging

--- libtrace/tracer.py
+++ libtrace/tracer.py
@@ -1,8 +1,11 @@
+import datetime
 from typing import Dict, Optional
 from libtrace.generate import generate_trace_id, generate_span_id
 from libtrace.span import Span
 from libtrace.trace import Trace
 from libevent import Client
+from libtrace import log
+
 """
 ADAPTED FROM Tracer CLASS AT
     https://github.com/honeycombio/beeline-python/trace.py
@@ -17,6 +20,39 @@ class Tracer(object):
         self._client = client
         self._trace = None
 
+    def finish_span(self, span) -> None:
+        # no span, nothing to do
+        if span is None:
+            return
+        # no trace, nothing to do
+        if not self._trace:
+            log('warning: span finished without an active trace')
+            return
+        if span.event:
+            for k, v in self._trace.fields.items():
+                if k not in span.event.fields():
+                    span.event.add_field(k, v)
+
+            duration = datetime.datetime.now() - span.start_time
+            duration_ms = duration.total_seconds() * 1000.0
+            span.event.add_field('duration_ms', duration_ms)
+
+            """
+            The honeycomb method is _run_hooks_and_send, and it's
+            run against the Span.
+            """
+            span.event.send()
+        else:  # something has gone horribly wrong
+            log('warning: span has no event, was it initialized correctly?')
+
+        # TBD: a bunch of checks to manage corrupt state
+
+        self._trace.stack.pop()
+
+    def finish_trace(self, span: Span) -> None:
+        self.finish_span(span)
+        self._trace = None
+
     def start_trace(self,
                     context: Optional[str] = None,
                     parent_span_id: Optional[str] = None) -> Span:
@@ -26,7 +62,9 @@ class Tracer(object):
 
     def start_span(self,
                    context: Optional[Dict] = None,
-                   parent_id: Optional[str] = None) -> Span:
+                   parent_id: Optional[str] = None) -> Optional[Span]:
+        if not self._trace:
+            return None
         span_id = generate_span_id()
         evt = self._client.new_event(data=context)
         if parent_id:
